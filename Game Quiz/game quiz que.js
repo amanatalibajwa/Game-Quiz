655dacc14fd61db6bcb28d4b5d730a4b0e4a992b
Amanat Ali Bajwa <143606819+amanatalibajwa@users.noreply.github.com>
2023-10-13 00:58:04 +0500

    Add files via upload

---
+++ Game Quiz/game quiz que.js
@@ -0,0 +1,58 @@
+// creating an array and passing the number, question, option , and answers
+let questions = [
+    {
+        numb : 1,
+       question:"1. What does HTML stands for?",
+       answer:"Hyper Text Markup Language",
+       options:[
+         "Hyper Text Markup Language",
+         "Hyper Text Preprocessor",
+         "Hyper Text Multiple Language",
+         "Hyper Tool Multi language",
+       ]
+    },
+    {
+        numb : 2,
+       question:"2. What does CSS stands for?",
+       answer:"Cascading Style Sheet",
+       options:[
+         "Common Style Sheet",
+         "Colorfull Style Sheet",
+         "Computer Style Sheet",
+         "Cascading Style Sheet",
+       ]
+    },
+    {
+        numb : 3,
+       question:"3. What does PHP stands for?",
+       answer:"Hyper Text Preprocessor",
+       options:[
+         "Hyper Text Programming",
+         "Hyper Text Preprocessor",
+         "Hyper Text Preprogramming",
+         "Hyper Tool Preprocessor",
+       ]
+    },
+    {
+        numb : 4,
+       question:"4. What does SQL stands for?",
+       answer:"Structured Query Language",
+       options:[
+         "Stylish Question Language",
+         "Stylesheet Query Language",
+         "Statement Question Language",
+         "Structured Query Language",
+       ]
+    },
+    {
+        numb : 5,
+       question:"5. What does XML stands for?",
+       answer:"Extensible Markup Language",
+       options:[
+         "Extensible Markup Language",
+         "Executable Text Preprocessor",
+         "Extra Multiple Language",
+         "Examine Multi language",
+       ]
+    },
+];
